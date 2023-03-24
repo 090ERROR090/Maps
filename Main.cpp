9759c30b37de59cbf1f0b67d7f34c6bd75bd155b
090ERROR090 <abhiram15431345@gmail.com>
2023-03-23 21:46:16 -0400

    out put test

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-    double x = 10;
-    double y = 5;
-    double z = (x + 10) / (3 * y);
+    int sales = 95000;
+    cout << "Sales: $" << sales << endl;
+    double state_tax = sales * 0.04;
+    cout << "State Tax: $" << state_tax << endl;
+    double county_tax = state_tax * 0.02;
+    cout << " County Tax: $" << county_tax << endl;
+
     
-    std::cout << z;
     return 0;
 }
