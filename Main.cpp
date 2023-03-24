3c35dcf4ab4b3cb0d2d543092cfd9a480785fd11
090ERROR090 <abhiram15431345@gmail.com>
2023-03-23 21:59:20 -0400

    next on

--- Main.cpp
+++ Main.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main(){
-    int sales = 95000;
-    cout << "Sales: $" << sales << endl;
-    double state_tax = sales * 0.04;
-    cout << "State Tax: $" << state_tax << endl;
-    double county_tax = state_tax * 0.02;
-    cout << " County Tax: $" << county_tax << endl;
-
-    
+    cout << "Enter a value for x and y: ";
+    double x;
+    double y;
+    cin >> x;
+    cin >> y;
+    cout << x + y;
     return 0;
 }
